9f69faf22ee84b225f5afb795006589e388883d0
Jonathan Shongo <3047468@ads.qub.ac.uk>
2018-05-16 10:47:52 +0100

    updated and removed unnecessary search functions

--- application/views/About_view.php
+++ application/views/About_view.php
@@ -6,7 +6,6 @@
 		</div>
 	</div>
 </main>
-<p>other text</p>
 <section class="text-center">
 	<div class="container">
 		<p>Personal and Institutional Collections (PIC) allows you to quickly browse and search individual print collections. Books and pamphlets in Special Collections can be challenging to locate in the general Library catalogue. By using PIC to browse and search titles you will find them in context, situated in their original collection, thereby increasing research possibilities whilst also narrowing the focus of search should this be required. A summary description of each collection is provided. For your convenience it is also possible to search across the collections available in PIC. All titles are fully catalogued and available in <a href="https://www.qub.ac.uk/lib"> Queen’s Library </a> catalogue, every record in PIC includes a link back to the Queen’s Library catalogue. There are 5 discrete print collections currently available in PIC but this number will continue to grow as we have 30 discrete print collections in our holdings at present.</p>
@@ -14,6 +13,3 @@
 		<p>Visit our website for information on how to access all <a href="https://www.qub.ac.uk/directorates/InformationServices/TheLibrary/SpecialCollections"> Special Collections </a> resources and services.</p>
 	</div>
 </section>
-
-
-
