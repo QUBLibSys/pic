28aef9d2bf3ccc7e3ee5962b7efe867c21966680
Arif Tahir <3051962@ads.qub.ac.uk>
2018-06-06 14:23:06 +0100

    Updated mailer using codeigniter syntax, removed PHPMailer

--- application/controllers/Email.php
+++ application/controllers/Email.php
@@ -16,41 +16,50 @@ class Email extends CI_Controller{
     }
 
     public function index(){
-        $subject = 'This is a test';
-        $message = $this->input->post('comment');
 
-        // $body = var_dump($_POST);
+        $config = [
+            'protocol' => 'smtp',
+            'smtp_host' => 'localhost',
+            'smtp_user' => '',
+            'smtp_pass' => '',
+            'smtp_crypto' => 'tls',
+            'newline' => "\r\n",
+            'smtp_port' => 25,
+            'mailtype' => 'html'
+        ];
+        $this->load->library('email', $config);        
+        $this->email->from('libsupport@qub.ac.uk');
+        $this->email->to('ariftahir17@gmail.com');
+        $this->email->subject('Test');
+        $this->email->message('This is the FIRST email');
+        $sent = $this->email->send();
+
+        if ($sent) {
+
+            $this->session->set_flashdata("email_sent","<div class='alert alert-success' role='alert'><p>Thank you, we will be in contact soon.</p></div>");
 
 
-    $result = $this->email
-    ->from($this->input->post('email'))
-    ->reply_to('yoursecondemail@somedomain.com')    // Optional, an account where a human being reads.
-    ->to('ariftahir17@gmail.com')
-    ->subject($subject)
-    ->message($message);
-    // ->send();
+            $this->email->from('libsupport@qub.ac.uk');
+            $this->email->to('ariftahir17@gmail.com');
+            $this->email->subject('Test 2');
+            $this->email->message('This is the SECOND email');
+            $sent = $this->email->send();
 
- if ($this->email->send()) {
 
-     $this->session->set_flashdata("email_sent","<div class='alert alert-success' role='alert'><p>Thank you, we will be in contact soon.</p></div>"); 
-     redirect(base_url().'contact');
-    // redirect(base_url().'contact', 'refresh');
 
-    } else {
-        // echo $this->email->print_debugger();
-    $this->session->set_flashdata("email_not_sent","<div class='alert alert-danger' role='alert'><p>Sorry, your email was not sent, please contact us if this problem persists.</p></div>"); 
-     redirect(base_url().'contact');
+            redirect(base_url().'contact');
+
+
+
+        } else {
+            // Use debugger for finding built in mailer errors
+            // echo $this->email->print_debugger();
+            $this->session->set_flashdata("email_not_sent","<div class='alert alert-danger' role='alert'><p>Sorry, your email was not sent, please contact by phone us if this problem persists.</p></div>");
+            redirect(base_url().'contact');
+        }
+        exit;
+
     }
-
-
-
-// var_dump($result);
-// echo '<br />';
-// echo $this->email->print_debugger();
-
-    exit;
-}
-
 }
 
 ?>
