8539a62b79f0ba80ea031bd695c644b662f2c8be
Arif Tahir <3051962@ads.qub.ac.uk>
2018-06-08 14:40:33 +0100

    Update bootstrap and JS

--- application/views/search_results_view.php
+++ application/views/search_results_view.php
@@ -181,8 +181,8 @@ $comma_separated_search_terms = implode(', ', array_filter($query_strings));
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
               <div>
-                <small class="text-muted" >Encore URL</small>
-                <p title="External Link" class="my-0"><a href="<?php echo $result['encore_url'] ?>" class="text-primary"><?php echo $result['encore_url'] ?></a></p>
+                <small class="text-muted">Encore URL</small>
+                <p title="External Link" id="encore_link" class="my-0"><a href="<?php echo $result['encore_url'] ?>" class="text-primary"><?php echo $result['encore_url'] ?></a></p>
               </div>
             </li>
             <li class="list-group-item d-flex justify-content-between lh-condensed">
