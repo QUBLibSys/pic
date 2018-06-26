ab1cf373c99cb42cc042f674533cd0833cb98d42
Johnny <j.shongo@qub.ac.uk>
2018-06-26 16:58:28 +0100

    add get full url helper function

---
+++ application/helpers/current_full_url_helper.php
@@ -0,0 +1,7 @@
+<?php
+function current_full_url()
+{
+    $CI =& get_instance();
+    $url = $CI->config->site_url($CI->uri->uri_string());
+    return $_SERVER['QUERY_STRING'] ? $url.'?'.$_SERVER['QUERY_STRING'] : $url;
+}
