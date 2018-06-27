8471c69f31b8eceb6c8fce597243d263069bca0c
Johnny <j.shongo@qub.ac.uk>
2018-06-27 12:16:29 +0100

    add qub logo. fixes #33

--- application/views/template/Footer.php
+++ application/views/template/Footer.php
@@ -29,10 +29,10 @@
                     <hr><p class="text"> &copy; <?php echo date("Y"); ?> Queen's University Belfast</p>
                 </div>
                 <div class="col-md-3">
-                    <style>
-
-                </style>
                 <p>Social</p>
+				<div class="mb-3">
+					<a href="http://www.qub.ac.uk" target="_blank"><img src="<?php base_url() ?>assets/img/qub_logo_white_sm.png" alt="Queen's University Belfast" title="Queen's University Belfast"></a>
+				</div>
                 <div class="social-icons">
                     <a href="https://www.facebook.com/specialcollectionsQUB"><img src="<?php echo base_url('assets/img/fb.png'); ?>" title="Special Collections Facebook" alt="Special Collections Facebook" width="30px"></a>
                     <a href="https://twitter.com/QUBSC"><img src="<?php echo base_url('assets/img/tw.png'); ?>" title="Special Collections Twitter" alt="Special Collections Twitter" width="30px"></a>
