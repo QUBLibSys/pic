10552005c4a0120e9a19d95eae85083e4f9c81c6
Johnny <j.shongo@qub.ac.uk>
2018-06-27 11:59:13 +0100

    Collection slides centered. Fixes #31

--- application/views/Home_view.php
+++ application/views/Home_view.php
@@ -96,7 +96,7 @@
 var swiper = new Swiper('.swiper-container', {
 	slidesPerView: 3,
 	spaceBetween: 30,
-	centeredSlides: true,
+	centeredSlides: false,
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
