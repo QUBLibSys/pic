58623c85ff4121834b864ee2febfdeed413c1ee1
Johnny <j.shongo@qub.ac.uk>
2018-06-28 11:48:06 +0100

    update social links url for results page and item view page

--- application/views/collection/Colltemplate.php
+++ application/views/collection/Colltemplate.php
@@ -26,45 +26,45 @@
 		
 	<div class="row">
         <div class="col-md-12">
-		<h2>Collection Samples</h2>
+		<h2>Collection results</h2>
 		<p class="text-muted"><small>Generated randomly</small></p>
           <div id="accordion" role="tablist">
             <!-- start foreach -->
-            <?php foreach ($collectionSample as $sample) : 
+            <?php foreach ($collectionSample as $result) : 
               $empty_value = 'Not Available';
-              $shelfmark = !empty($sample['marc_099_coll_ident']) ? $sample['marc_099_coll_ident'] : $sample['marc_090_coll_ident'] ;
+              $shelfmark = !empty($result['marc_099_coll_ident']) ? $result['marc_099_coll_ident'] : $result['marc_090_coll_ident'] ;
               ?>
 
               <div class="card list-view-brand">
                 <div class="card-header" role="tab">
                   <p title="Click to expand">
-                    <a data-toggle="collapse" id="#<?php echo $sample['record_id'] ?>" href="#<?php echo $sample['record_id'] ?>" aria-expanded="false" aria-controls="<?php echo $sample['record_id'] ?>" class="collapsed ga-accordian-tag" value="xxx">
-                      <?php echo $shelfmark ?> | <strong  id="accordian-title"><?php echo mb_strimwidth($sample['marc_245_title_stmt'], 0, 100, "...")?></strong>
+                    <a data-toggle="collapse" id="#<?php echo $result['record_id'] ?>" href="#<?php echo $result['record_id'] ?>" aria-expanded="false" aria-controls="<?php echo $result['record_id'] ?>" class="collapsed ga-accordian-tag" value="xxx">
+                      <?php echo $shelfmark ?> | <strong  id="accordian-title"><?php echo mb_strimwidth($result['marc_245_title_stmt'], 0, 100, "...")?></strong>
                     </a>
                   </p>
                   <div class="item-tags">
-                    <span class="badge tag-outline"><?php echo $sample['name'] ?></span>
+                    <span class="badge tag-outline"><?php echo $result['name'] ?></span>
                   </div>
                 </div>
 
-                <div id="<?php echo $sample['record_id'] ?>" class="collapse" role="tabpanel" data-parent="#accordion" style="">
+                <div id="<?php echo $result['record_id'] ?>" class="collapse" role="tabpanel" data-parent="#accordion" style="">
                   <div class="card-body">
                     <ul class="list-group mb-3">
                       <li class="list-group-item d-flex justify-content-between lh-condensed">
                         <div>
                           <small class="text-muted">Title</small>
-                          <p class="my-0"><?php echo $sample['marc_245_title_stmt'];?></p>
+                          <p class="my-0"><?php echo $result['marc_245_title_stmt'];?></p>
                         </div>
                       </li>
                       <li class="list-group-item d-flex justify-content-between lh-condensed">
                         <div>
                           <small class="text-muted">Language</small>
-                          <p class="my-0"><?php echo $sample['marc_008_lang'];?></p>
+                          <p class="my-0"><?php echo $result['marc_008_lang'];?></p>
                         </div>
                       </li>
                       <li class="list-group-item d-flex justify-content-between lh-condensed">
                         <div>
-                          <a class="btn btn-outline primary btn-sm" href="<?php echo base_url() . 'collection/' . $items[0]['url'] ?>/item/<?php echo $sample['record_id'] ?>" role="button">View Full Record</a>
+                          <a class="btn btn-outline primary btn-sm" href="<?php echo base_url() . 'collection/' . $items[0]['url'] ?>/item/<?php echo $result['record_id'] ?>" role="button">View Full Record</a>
                         </div>
                       </li>
                     </ul>
@@ -78,7 +78,12 @@
         </div> <!-- end row -->
 		<div class="row">
 			<div class="col-md-12">
-				<p class="text-center"><a href="<?php echo base_url(). 'search/results?coll_id=' . $sample{'coll_id'} ?>" title="View Full <?php echo $sample['name'] ?> Collection"> View Full <?php echo $sample['name'] ?> Collection</a></p>
+				<p class="text-center"><a href="<?php echo base_url(). 'search/results?coll_id=' . $result{'coll_id'} ?>" title="View Full <?php echo $result['name'] ?> Collection"> View Full <?php echo $result['name'] ?> Collection</a></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<?php social_share_links($result);?>
 			</div>
 		</div>
 	</div> <!-- /container -->
